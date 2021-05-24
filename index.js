c91cf3d5b931409c3677d7cf50b1471ebe229747
baroxfitxhcmus <pxbao.itus@gmail.com>
2021-05-24 12:06:41 +0700

    update package.json and index.js

--- index.js
+++ index.js
@@ -5,21 +5,45 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-
+const cors = require('cors');
+const morgan = require('morgan');
 
 // Import module local file
 const loginAPI = require('./src/apis/login.api');
-
+const accountAPI = require('./src/apis/account.api');
 
 // Set port
 const app = express();
 const normalizePort = (port) => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 3500);
 
+
 // Set up server
+app.use(
+    cors({
+            // Configures the Access-Control-Allow-Origin
+        origin: process.env.CORS_ORIGIN,
+
+        // Configures the Access-Control-Allow-Methods
+        methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+
+        //Configures the Access-Control-Allow-Headers
+        allowedHeaders:
+            'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept',
+
+        // Configures the Access-Control-Allow-Credentials
+        credentials: true,
+
+        //Configures the Access-Control-Expose-Headers
+        exposedHeaders: 'Content-Range,X-Content-Range,Authorization',
+
+        // Provides a status code to use for successful OPTIONS requests
+        optionsSuccessStatus: 200,
+    })
+  );
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
-
+app.use(cookieParser());
 
 // Connect to MongoDB with mongoose 
 const mongoose = require('mongoose');
@@ -36,3 +60,5 @@ app.listen(PORT,()=>{
 // API chuc nang login
 app.use('/auth/login', loginAPI);
 
+// API chuc nang SignUp
+app.use('/signup',accountAPI);
