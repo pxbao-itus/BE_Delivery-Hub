1ba07f90f52758ca23e6003ccbcc50634f7f46fe
baroxfitxhcmus <pxbao.itus@gmail.com>
2021-06-24 16:53:47 +0700

    change account model add type, lock

--- src/models/account.model/account.model.js
+++ src/models/account.model/account.model.js
@@ -3,10 +3,12 @@ const md5 = require('md5');
 
 const accountSchema = new mongoose.Schema({
     email : String,
-    password : String
+    password : String,
+    type: Number,
+    lock: Boolean
 },{
     versionKey: false
 })
 
-const Account = mongoose.model('Account',accountSchema,'accounts');
+const Account = mongoose.model('Account',accountSchema,'Accounts');
 module.exports = Account;
