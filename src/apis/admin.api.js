71e30939a8a41ad9072e22cd8ed5e1cc171fe187
baroxfitxhcmus <pxbao.itus@gmail.com>
2021-07-08 17:47:06 +0700

    create get all order api for admin

--- src/apis/admin.api.js
+++ src/apis/admin.api.js
@@ -7,4 +7,6 @@ adminAPI.post('/user/lock', accountController.updateAccountStatus);
 
 adminAPI.get('/user', adminController.getUser);
 
+adminAPI.get('/order', adminController.getOrder);
+
 module.exports = adminAPI;
--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,6 @@
 const Account = require('../models/account.model/account.model');
+const IndividualOrderModel = require('../models/order.model/individual_customer.order.model');
+const SaleBusinessOrderModel = require('../models/order.model/sale_business.order.model');
 
 const getUser = async (req, res, next) => {
     try{
@@ -9,7 +11,17 @@ const getUser = async (req, res, next) => {
     }
 }
 
+const getOrder = async (req, res, next) => {
+    try{
+        const individualOrder = await IndividualOrderModel.find({});
+        const saleBusinessOrder = await SaleBusinessOrderModel.find({});
+        return res.status(200).json({individualOrder, saleBusinessOrder});
+    } catch (error) {
+        return res.status(400).json({message: "Failed"});
+    }
+}
 
 module.exports = {
     getUser,
+    getOrder,
 }
